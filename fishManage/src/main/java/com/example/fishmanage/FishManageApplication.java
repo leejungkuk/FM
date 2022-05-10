4b95c80153b8e40d74098ec1e25f0e82021a24a5
leejungkuk <99408655+leejungkuk@users.noreply.github.com>
2022-05-10 11:14:32 +0900

    Add files via upload

---
+++ fishManage/src/main/java/com/example/fishmanage/FishManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishManageApplication.class, args);
+    }
+
+}
---
+++ fishManage/src/test/java/com/example/fishmanage/FishManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
